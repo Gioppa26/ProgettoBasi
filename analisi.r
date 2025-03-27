929dca655479aa37924ffdb7e09bb34c368e65d0
Gioppa26 <gio26.pantarotto@gmail.com>
2025-03-27 19:22:29 +0100

    Analisi R
    Aggiunta file analisi.r contenente il codice di popolamento del database presente in localhost

--- analisi.r
+++ analisi.r
@@ -1,21 +1,46 @@
-library("RPostgreSQL")
+library("DBI")
+library("RPostgres")
 
-drv <- dbDriver("PostgreSQL")
+pg_connection <- dbConnect(
+  RPostgres::Postgres(),
+  dbname = "registro_automobilistico",
+  host= "127.0.0.1",
+  port="5432",
+  user="postgres",
+  password="admin")
 
-con <- dbConnect(drv,
-                 dbname = "registro_automobilistico",
-                 host= "<indirizzo IP dbserver>",
-                 port="5432",
-                 user="postgres",
-                 password="<passwd>")
+fabbriche <- read.csv("dataset/fabbriche.csv",header = TRUE)
+dbWriteTable(pg_connection,name="fabbrica",value = fabbriche, append = T, row.names = F)
 
-dbWriteTable( con,
-              name=c("<schema>", "<tabella>"),
-              value=<data_frame>,
-              append=<T=F>,
-              row.names=<T=F>)
+modelli <- read.csv("dataset/modelli.csv",header = TRUE)
+dbWriteTable(pg_connection,name="modello",value = modelli, append = T, row.names = F)
 
-fabbriche_df <- data.frame(
-  id_fabbrica = sample(1:10,1,replace = F),
-  nome = sample()
-)
+combustibili <- read.csv("dataset/combustibili.csv",header = TRUE)
+dbWriteTable(pg_connection,name="combustibile",value = combustibili, append = T, row.names = F)
+
+proprietari <- read.csv("dataset/proprietari.csv",header = TRUE)
+dbWriteTable(pg_connection,name="proprietario",value = proprietari, append = T, row.names = F)
+
+privati <- read.csv("dataset/privati.csv",header = TRUE)
+dbWriteTable(pg_connection,name="privato",value = privati, append = T, row.names = F)
+
+societa <- read.csv("dataset/societa.csv",header = TRUE)
+dbWriteTable(pg_connection,name="societa",value = societa, append = T, row.names = F)
+
+veicoli <- read.csv("dataset/veicoli.csv",header = TRUE)
+dbWriteTable(pg_connection,name="veicolo",value = veicoli, append = T, row.names = F)
+
+proprietari_passati <- read.csv("dataset/proprietari_passati.csv",header = TRUE)
+dbWriteTable(pg_connection,name="proprietari_passati",value = proprietari_passati, append = T, row.names = F)
+
+automobili <- read.csv("dataset/automobili.csv",header = TRUE)
+dbWriteTable(pg_connection,name="automobile",value = automobili, append = T, row.names = F)
+
+camion <- read.csv("dataset/camion.csv",header = TRUE)
+dbWriteTable(pg_connection,name="camion",value = camion, append = T, row.names = F)
+
+ciclomotori <- read.csv("dataset/ciclomotori.csv",header = TRUE)
+dbWriteTable(pg_connection,name="ciclomotore",value = ciclomotori, append = T, row.names = F)
+
+rimorchi <- read.csv("dataset/rimorchi.csv",header = TRUE)
+dbWriteTable(pg_connection,name="rimorchio",value = rimorchi, append = T, row.names = F)
