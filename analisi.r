1dcb93dc117dd60a1d55c1830b42fc1bc2d4645c
rolandgjopalaj <78623731+rolandgjopalaj@users.noreply.github.com>
2025-04-04 09:56:01 +0200

    Fixed analisi.r

--- analisi.r
+++ analisi.r
@@ -4,34 +4,55 @@ library("ggplot2")
 
 pg_connection <- dbConnect(
   RPostgres::Postgres(),
-  dbname = "registroAuto",
+  dbname = "registro_m",
   host= "127.0.0.1",
   port="5432",
   user="postgres",
-  password="Giovanni26!")
+  password="password")
 
+
+# grafico che visualizza il numero di modellio per ogni fabbrica##############
 result <- dbGetQuery(pg_connection,
                      "SELECT f.nome AS fabbriche,count(m.id_modello) as numeroModelli
                       FROM fabbrica as f, modello as m
                       WHERE f.id_fabbrica = m.fabbrica_di_produzione
                       GROUP BY f.nome")
 
-result <- na.omit(result)
-modelli <- list(result$numeromodelli)
 
-barplot(
-  modelli,
-  names.arg = result$fabbriche,
-  xlab = "Fabbriche",
-  ylab = "Numero Modelli",
-  main = "Numero di Modelli per Fabbrica",
-  col = rainbow(length(result$fabbriche)), # Colori arcobaleno per le barre
-  ylim = c(0, max(result$numeromodelli) + 2) # Imposta il limite superiore dell'asse y
-)
+# Creazione del grafico a barre con ggplot2
+ggplot(data = result, aes(x = fabbriche, y = numeromodelli, fill = numeromodelli)) +
+  geom_bar(stat = "identity", color = "black") +
+  xlab("Fabbriche") +
+  ylab("Numero di Modelli") +
+  ggtitle("Numero di Modelli per Fabbrica") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_gradient(low = "lightblue", high = "darkblue")
+#########################################################################
 
-text(
-  x = seq(1, length(result$fabbriche) * 1.2, by = 1.2),
-  y = result$numeromodelli + 0.5,
-  labels = result$numeromodelli,
-  cex = 0.8 # Dimensione del testo
-)
+
+## Distribuzione dei Veicoli per Categoria ########################################
+result <- dbGetQuery(pg_connection,
+                     "SELECT
+                      CASE
+                          WHEN targa IN (SELECT targa FROM automobile) THEN 'automobile'
+                          WHEN targa IN (SELECT targa FROM camion) THEN 'camion'
+                          WHEN targa IN (SELECT targa FROM ciclomotore) THEN 'ciclomotore'
+                          WHEN targa IN (SELECT targa FROM rimorchio) THEN 'rimorchio'
+                          ELSE 'altro'
+                        END AS categoria,
+                        COUNT(*) AS numero_veicoli
+                      FROM veicolo
+                      GROUP BY categoria")
+
+ggplot(result, aes(x = "", y = numero_veicoli, fill = categoria)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  labs(x = NULL, y = NULL, fill = "Categoria", title = "Distribuzione dei Veicoli per Categoria") +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5))
+#########################################################################
+
+
+
+# Disconnessione dal database
+dbDisconnect(pg_connection)
